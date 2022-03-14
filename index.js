d5d0ce81e13a6dfaff8db383094f3bc07bc7f0b9
William Luck <luck.william.h@gmail.com>
2022-03-14 13:18:31 -0500

    commit tree 1d5408d552fff43febb948fba93e0fd8dd7714eb

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  if (value <= 400) {
+    return "This one is on me!"
+  } else if ((value > 2000) && (value <= 2500 )) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === "NYC") ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) { 
+    case "generous": 
+      return "Thank you so much."
+    case "not as generous": 
+      return "Thank you."
+    default :
+      return "Bye."
+  }
 }
